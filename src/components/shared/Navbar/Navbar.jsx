cd3da1c33075c4a7f1b4a819dc57bc6bd37df2bc
Ruhit Paul <ruhitpaul18@gmail.com>
2026-06-01 22:29:29 +0600

    feat: update Navbar layout for better responsiveness on medium screens

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white shadow-sm border-b border-gray-100 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-20">
+        <div className="flex md:justify-between items-center h-20">
           <div className="md:hidden flex items-center">
             <Drawer>
               <Button isIconOnly variant="light" aria-label="Open Menu">
